2271e2e91fe708a59d15b250b953f355f1fa297c
vatrostox <vatroslav.celan@everstox.com>
2026-04-07 13:06:31 +0200

    gdrive listener less common port

--- thoa/config/config.py
+++ thoa/config/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     THOA_API_DEBUG: bool = False
     THOA_API_TIMEOUT: int = 30
     THOA_GDRIVE_CALLBACK_HOST: str = "127.0.0.1"
-    THOA_GDRIVE_CALLBACK_PORT: int = 8765
+    THOA_GDRIVE_CALLBACK_PORT: int = 54389
     THOA_GDRIVE_OPEN_BROWSER: bool = True
 
     class Config:
